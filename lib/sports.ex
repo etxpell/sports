8173db0600317e44319f52ef15cfbbca54ec31e0
Peter Lundell <peter.lundell@derivco.se>
2017-10-12 14:03:13 +0200

    first

---
+++ lib/sports.ex
@@ -0,0 +1,18 @@
+defmodule Sports do
+  @moduledoc """
+  Documentation for Sports.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sports.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sports_test.exs
@@ -0,0 +1,8 @@
+defmodule SportsTest do
+  use ExUnit.Case
+  doctest Sports
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
